03b45fd506c35901c089494235b72766204a7920
James Hu <hello@james.hu>
2015-09-21 00:35:56 -0700

    Working version

--- mpp.rb
+++ mpp.rb
@@ -1,9 +1,19 @@
 #!/usr/bin/env ruby
 
 require 'pathname'
+require 'fileutils'
+
+# Bundler
+require 'rubygems'
+require 'bundler/setup'
+
+require 'active_support/all'
+require 'commander/import'
 
 # Rename paths with only ASCII characters
-def asciify_paths(paths)
+def asciify_paths(label, paths)
+  puts "Asciifying paths for #{label}..."
+
   fallback = { 'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A',
                'Å' => 'A', 'Æ' => 'AE', 'Ç' => 'C', 'È' => 'E', 'É' => 'E',
                'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I',
@@ -15,9 +25,9 @@ def asciify_paths(paths)
                'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ñ' => 'n',
                'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o',
                'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ü' => 'u',
-               'ý' => 'y', 'ÿ' => 'y' }
+               'ý' => 'y', 'ÿ' => 'y', '’' => '\'' }
 
-  movie_paths.each do |path|
+  paths.each do |path|
     ascii = true
 
     # Check if ASCII
@@ -30,7 +40,10 @@ def asciify_paths(paths)
 
     unless ascii
       ascii_path = path.encode('ASCII', fallback: lambda { |c| fallback[c] || '?' })
-      puts "#{path} => #{ascii_path}"
+
+      puts "Moving #{path} to #{ascii_path}..."
+
+      FileUtils.mv path, ascii_path
     end
   end  
 end
@@ -39,19 +52,37 @@ end
 def run_subliminal(label, paths, options = {})
   options.reverse_merge!(language: "en")
 
-  option_args = options.each_with_object([]) { |(k, v), a| a << "--#{key} #{v}" }.join(' ')
+  option_args = options.each_with_object([]) { |(k, v), a| a << "--#{k} #{v}" }.join(' ')
   path_args = paths.map { |p| "\"#{p}\"" }.join(' ')
 
-  puts "Running subliminal on #{label}..."
+  puts "Fetching subtitles for #{label}..."
 
   system "subliminal download #{option_args} #{path_args}" 
-
-  puts "Finished running subliminal on #{label}!"
 end
 
 media_root = Pathname.new("/mnt/media")
-tv_paths = Dir.glob(media_root.join("TV/*/Season*"))
-movie_paths = Dir.glob(media_root.join("Movies/*"))
 
-#run_subliminal("TV Shows", tv_paths, age: "30d")
-#run_subliminal("Movies", movie_paths)
+program :name, "mpp"
+program :version, "1.0.0"
+program :description, "Media post processor"
+
+command :tv do |c|
+  c.action do |args, options|
+    options.default age: "10d"
+
+    label = "TV Shows"
+    tv_paths = Dir.glob(media_root.join("TV/*/Season*"))
+
+    run_subliminal(label, tv_paths, age: options.age) 
+  end 
+end
+
+command :movies do |c|
+  c.action do |args, options|
+    label = "Movies"
+    movie_paths = Dir.glob(media_root.join("Movies/*"))
+
+    asciify_paths(label, movie_paths)
+    run_subliminal(label, movie_paths)
+  end
+end
